13001b00006fb0d0faddfa3dba9e0a7a966ca93e
Kamil Dominko <kamil.dominko@gmail.com>
2023-08-29 13:49:11 +0200

    Dodanie głównej klasy gry oraz utworzenie pustego okna pygame.

--- main.py
+++ main.py
@@ -5,3 +5,35 @@ statki. Jeżeli wrogi statek dotknie gracza lub zejdzie poniżej określonej
 wysokości ekranu, gracz traci jedno życie. Po zestrzeleniu wrogiego statku
 gracz dostaje jeden punkt, po zniszczeniu wszystkich statków w fali dostaje
 dziesięć punktów"""
+
+import sys
+
+import pygame
+
+
+class Game:
+    """Główna klasa gry przeznaczona do zarządzania zasobami i sposobem
+    działania gry."""
+
+    def __init__(self):
+        """Inicjalizacja gry i utworzenie jej zasobów."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Inwazja Obcych")
+
+    def run(self):
+        "Rozpoczęcie głównej pętli gry."
+        while True:
+            # Oczekiwanie na naciśnięcie klawisza lub przycisku myszy.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # Wyświetlenie ostatnio zmodyfikowanego ekranu
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Utworzenie egzemplarza gry i jej uruchomienie
+    game = Game()
+    game.run()
