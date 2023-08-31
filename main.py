bb2351c536f9b07673f03f81220945662ff881b0
Kamil Dominko <kamil.dominko@gmail.com>
2023-08-31 13:57:44 +0200

    Dodanie kasowania pocisków wylatujących poza ekran gry.

--- main.py
+++ main.py
@@ -80,6 +80,11 @@ class Game:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
             self._update_screen()
 
 
