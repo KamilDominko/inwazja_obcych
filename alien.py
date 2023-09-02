7fcbc9c3b26db3bc04da22b01d571a2455ec228c
Kamil Dominko <kamil.dominko@gmail.com>
2023-09-02 15:49:13 +0200

    Dodanie klasy obcego i jego grafiki.

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Klasa przedstawiającego pojedyńczego obcego we flocie."""
+
+    def __init__(self, game):
+        """Inicjalizacja obcego i zdefiniowanie jego położenia początkowego."""
+        super().__init__()
+        self.screen = game.screen
+
+        # Wczytanie obrazu obcego i zdefiniowanie jego atrybutu rect.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Umieszczenie nowego obcego w pobliżu lewego rogu ekranu.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Przechowywanie dokładnego poziomego położenia obcego.
+        self.x = float(self.rect.x)
